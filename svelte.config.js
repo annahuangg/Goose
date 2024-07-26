d4e134d9f5c2cdc4508d9e0fe840938bd93aa06c
annahuangg <annahuang5668@gmail.com>
2024-07-26 14:23:38 -0500

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+// import sveltePreprocess from 'svelte-preprocess';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   preprocess: sveltePreprocess(),
+
+//   kit: {
+//     adapter: adapter(),
+//   }
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-vercel';
 import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +20,12 @@ const config = {
   preprocess: sveltePreprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // Vercel-specific options (if any) go here
+    }),
+    paths: {
+      base: '' // Leave empty for local development
+    }
   }
 };
 
