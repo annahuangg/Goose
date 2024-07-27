2c33d3afbb3972dda86ea355f12f4778a26d7373
Anna <44736708+annahuangg@users.noreply.github.com>
2024-07-27 02:14:13 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,21 +12,37 @@
 
 // export default config;
 
-import adapter from '@sveltejs/adapter-vercel';
-import sveltePreprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-vercel';
+// import sveltePreprocess from 'svelte-preprocess';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   preprocess: sveltePreprocess(),
+
+//   kit: {
+//     adapter: adapter({
+//       // Vercel-specific options (if any) go here
+//     }),
+//     paths: {
+//       base: '' // Leave empty for local development
+//     }
+//   }
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: sveltePreprocess(),
-
-  kit: {
-    adapter: adapter({
-      // Vercel-specific options (if any) go here
-    }),
-    paths: {
-      base: '' // Leave empty for local development
-    }
-  }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
