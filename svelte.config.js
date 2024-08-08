a49dd6e2319ba4eb914d93c2a91619bbf6b1dbef
Anna <44736708+annahuangg@users.noreply.github.com>
2024-08-08 18:54:09 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -40,7 +40,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/Goose' // Default to '/Goose'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
